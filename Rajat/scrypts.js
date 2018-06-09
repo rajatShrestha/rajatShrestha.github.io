7f4714086bed72240410e1b54e48080f445cd1fe
rajatShrestha <38484079+rajatShrestha@users.noreply.github.com>
2018-06-09 21:49:13 +0545

    2149 0906

--- Rajat/scrypts.js
+++ Rajat/scrypts.js
@@ -3,7 +3,6 @@ function form() {
   var name = document.forms["contact"]["name"].value;
   var age = document.forms["contact"]["age"].value;
   var email = document.forms["contact"]["email"].value;
-  var gender = document.forms["contact"]["gender"].value;
   var message = document.forms["contact"]["message"].value;
   
   if (name == "" ||  email == "" || message == ""){
@@ -30,11 +29,13 @@ window.onload = function typeWriter() {
   }
 }
 
-// pop up messages
-function yes() {
-  alert("Thank you for your time.");
-}
 
-function no() {
-  alert("May your life be full of corurseworks!");
+//for the navbar
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+      x.className += " responsive";
+  } else {
+      x.className = "topnav";
+  }
 }
