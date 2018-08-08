7de3116bbd20b9d211072b7bb1e1225e4702b508
rajatShrestha <38484079+rajatShrestha@users.noreply.github.com>
2018-08-08 06:34:32 +0545

    php

---
+++ php_practice.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>webpage archives</title>
+		<style>
+
+			a {
+				padding: 40px;
+				font-family: 'Helvetica', sans-serif;
+				font-size: 7vw;
+				text-decoration: none;
+			}
+		</style>
+	</head>
+	<body>
+		<?php  
+			
+
+		?>
+	</body>
+</html>
